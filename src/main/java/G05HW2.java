ede8f3adae36494df8e770a0df0ac6cd99c2b0d5
Mikele Milia <m.milia@noonic.com>
2020-05-15 22:36:36 +0200

    added some comments

--- src/main/java/G05HW2.java
+++ src/main/java/G05HW2.java
@@ -1,33 +1,41 @@
 import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
-import java.io.BufferedReader;
-import java.io.FileReader;
+
 import java.util.Arrays;
-import java.util.StringTokenizer;
 import java.util.*;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-class Point implements Comparable<Point> {
-    double x, y;
-
-    public int compareTo(Point p) {
-        if (this.x == p.x) {
-            return 0;
-        } else if(this.x<p.x){
-            return -1;
-        }
-        else return 1;
-    }
-
-    public String toString() {
-        return "(" + x + "," + y + ")";
-    }
-
-}
-
 public class G05HW2 {
+
+    // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+    // Auxiliary methods
+    // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+    public static Vector strToVector(String str) {
+        String[] tokens = str.split(",");
+        double[] data = new double[tokens.length];
+        for (int i = 0; i < tokens.length; i++) {
+            data[i] = Double.parseDouble(tokens[i]);
+        }
+        return Vectors.dense(data);
+    }
+
+    public static ArrayList<Vector> readVectorsSeq(String filename) throws IOException {
+        if (Files.isDirectory(Paths.get(filename))) {
+            throw new IllegalArgumentException("readVectorsSeq is meant to read a single file.");
+        }
+        ArrayList<Vector> result = new ArrayList<>();
+        Files.lines(Paths.get(filename))
+                .map(str -> strToVector(str))
+                .forEach(e -> result.add(e));
+        return result;
+    }
+
+    // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+    // Developed methods
+    // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
     private static double distanceFromSet(Vector x, ArrayList<Vector> S) {
         double d = Double.NEGATIVE_INFINITY;                        // initialize to infinity
@@ -48,59 +56,6 @@ public class G05HW2 {
         }
         return dist;
     }
-
-    public static double cross(Point O, Point A, Point B) {
-        return (A.x - O.x) *(B.y - O.y) - (A.y - O.y) *(B.x - O.x);
-    }
-
-    public static Point[] convex_hull(Point[] P) {
-
-        if (P.length > 1) {
-            int n = P.length, k = 0;
-            Point[] H = new Point[2 * n];
-
-            Arrays.sort(P);
-
-            // Build lower hull
-            for (int i = 0; i < n; ++i) {
-                while (k >= 2 && cross(H[k - 2], H[k - 1], P[i]) <= 0.0)
-                    k--;
-                H[k++] = P[i];
-            }
-
-            // Build upper hull
-            for (int i = n - 2, t = k + 1; i >= 0; i--) {
-                while (k >= t && cross(H[k - 2], H[k - 1], P[i]) <= 0)
-                    k--;
-                H[k++] = P[i];
-            }
-            if (k > 1) {
-                H = Arrays.copyOfRange(H, 0, k - 1); // remove non-hull vertices after k; remove k - 1 which is a duplicate
-            }
-            return H;
-        } else if (P.length <= 1) {
-            return P;
-        } else {
-            return null;
-        }
-    }
-
-    public static double exactMPD_forConvexHull(Point[] P)
-    {
-        double dist = Double.NEGATIVE_INFINITY;
-        for(Point x : P)
-        {
-            for(Point y : P)
-            {
-                double d = Math.sqrt(Math.pow(x.x-y.x,2)+Math.pow(x.y-y.y,2));
-                if(d>dist) dist=d;
-            }
-        }
-
-        return dist;
-    }
-
-
 
     public static double twoApproxMPD(ArrayList<Vector> S, int k) {
         long seed = 1218949;
@@ -131,7 +86,6 @@ public class G05HW2 {
         return dist;
     }
 
-    // K-CENTER MPD
     private static ArrayList<Vector> kCenterMPD(ArrayList<Vector> PS, int k) {
         ArrayList<Vector> P;
         ArrayList<Vector> C = new ArrayList<>();
@@ -156,76 +110,114 @@ public class G05HW2 {
         return C;
     }
 
-    // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-    // Auxiliary methods
-    // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+    // The following methods were developed to speed up the running
+    // time of exactMPD method, since it takes hours to process the
+    // medium and large datasets
 
-    public static Vector strToVector(String str) {
-        String[] tokens = str.split(",");
-        double[] data = new double[tokens.length];
-        for (int i = 0; i < tokens.length; i++) {
-            data[i] = Double.parseDouble(tokens[i]);
-        }
-        return Vectors.dense(data);
+    public static double cross(Point O, Point A, Point B) {
+        return (A.x - O.x) * (B.y - O.y) - (A.y - O.y) * (B.x - O.x);
     }
 
-    public static ArrayList<Vector> readVectorsSeq(String filename) throws IOException {
-        if (Files.isDirectory(Paths.get(filename))) {
-            throw new IllegalArgumentException("readVectorsSeq is meant to read a single file.");
+    public static Point[] convexHull(Point[] P) {
+
+        if (P.length > 1) {
+            int n = P.length, k = 0;
+            Point[] H = new Point[2 * n];
+
+            Arrays.sort(P);
+
+            // Build lower hull
+            for (Point point : P) {
+                while (k >= 2 && cross(H[k - 2], H[k - 1], point) <= 0.0)
+                    k--;
+                H[k++] = point;
+            }
+
+            // Build upper hull
+            for (int i = n - 2, t = k + 1; i >= 0; i--) {
+                while (k >= t && cross(H[k - 2], H[k - 1], P[i]) <= 0)
+                    k--;
+                H[k++] = P[i];
+            }
+            if (k > 1) {
+                H = Arrays.copyOfRange(H, 0, k - 1); // remove non-hull vertices after k; remove k - 1 which is a duplicate
+            }
+            return H;
+        } else {
+            return P;
         }
-        ArrayList<Vector> result = new ArrayList<>();
-        Files.lines(Paths.get(filename))
-                .map(str -> strToVector(str))
-                .forEach(e -> result.add(e));
-        return result;
+        /*else if (P.length <= 1) {             TODO: questo ramo può essere evitato, è sempre vero
+            return P;
+        } else {
+            return null;
+        }*/
     }
+
+    public static double exactMPD_convexHull(Point[] P) {
+        double dist = Double.NEGATIVE_INFINITY;
+        for (Point x : P) {
+            for (Point y : P) {
+                double d = Math.sqrt(Math.pow(x.x - y.x, 2) + Math.pow(x.y - y.y, 2));
+                if (d > dist) dist = d;
+            }
+        }
+
+        return dist;
+    }
+
+    // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+    // Code
+    // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
     public static void main(String[] args) {
 
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-        // Reading points from a file whose name is provided as args[0]
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-
-        String filename = args[0];
-        ArrayList<Vector> inputPoints;
+        String filename = args[0];      // dataset name
+        long start, end;                // helps with time statistics
 
         try {
 
-            inputPoints = readVectorsSeq(filename);
+            ArrayList<Vector> inputPoints = readVectorsSeq(filename);   // gathering of all dataset points
+            int k = Integer.parseInt(args[1]);                          // set the value of k
 
-            int k = Integer.parseInt(args[1]);
+            //-----------------------------------------------------------------------------
+            System.out.println("Start conversion from Vector to Point");
+            start = System.currentTimeMillis();
 
-            int n = inputPoints.size();
-
-            System.out.println("Start Point conversion");
-            long start = System.currentTimeMillis();
-            Point[] p = new Point[n];
-            for(int i = 0; i<n ; i++ )
-            {
+            Point[] p = new Point[inputPoints.size()];
+            for (int i = 0; i < inputPoints.size(); i++) {
                 Vector v = inputPoints.get(i);
                 double[] o = v.toArray();
-                p[i]= new Point();
+                p[i] = new Point();
                 p[i].x = o[0];
                 p[i].y = o[1];
             }
 
-            long end = System.currentTimeMillis();
-            System.out.println("Input Size= " + p.length);
-            System.out.println("conversion to Point finish in= " + (end - start) + "ms" );
+            end = System.currentTimeMillis();
+
+            System.out.println("\tInput size : " + p.length);
+            System.out.println("\tConversion to Point finish in : " + (end - start) + "ms");
 
             System.out.println("Start finding Convex Hull");
 
             start = System.currentTimeMillis();
-            Point[] hull = convex_hull(p).clone();
-            System.out.println("Finding Convex Hull diameter.");
-            System.out.println("Number of vertices in Convex Hull= " + hull.length);
-            double exactMPD_forConvexHull = exactMPD_forConvexHull(hull);
+            Point[] hull = convexHull(p).clone();
+            System.out.println("\tFinding Convex Hull diameter : ");
+            System.out.println("\tNumber of vertices in Convex Hull : " + hull.length + "\n");
+            double exactMPD_convexHull = exactMPD_convexHull(hull);
             end = System.currentTimeMillis();
 
-            System.out.println("EXACT ALGORITHM");
-            System.out.println("Max distance = " + exactMPD_forConvexHull);
-            System.out.println("Running time = " + (end - start) + "ms");
-
+            System.out.println("EXACT ALGORITHM (with Convex Hull)");
+            System.out.println("Max distance = " + exactMPD_convexHull);
+            System.out.println("Running time = " + (end - start) + "ms\n");
+            //-----------------------------------------------------------------------------
+//            start = System.currentTimeMillis();
+//            double exactMPD = exactMPD(inputPoints);
+//            end = System.currentTimeMillis();
+//
+//            System.out.println("EXACT ALGORITHM");
+//            System.out.println("Max distance = " + exactMPD);
+//            System.out.println("Running time = " + (end - start) + "ms\n");
+            //-----------------------------------------------------------------------------
             start = System.currentTimeMillis();
             double twoApproxMPD = twoApproxMPD(inputPoints, k);
             end = System.currentTimeMillis();
@@ -233,21 +225,44 @@ public class G05HW2 {
             System.out.println("2-APPROXIMATION ALGORITHM");
             System.out.println("k = " + k);
             System.out.println("Max distance = " + twoApproxMPD);
-            System.out.println("Running time = " + (end - start) + "ms");
-
+            System.out.println("Running time = " + (end - start) + "ms\n");
+            //-----------------------------------------------------------------------------
             start = System.currentTimeMillis();
             ArrayList<Vector> kCenterMPD = kCenterMPD(inputPoints, k);
             double exact_kCenterMPD = exactMPD(kCenterMPD);
             end = System.currentTimeMillis();
+
             System.out.println("k-CENTER-BASED ALGORITHM");
             System.out.println("k = " + k);
             System.out.println("Max distance = " + exact_kCenterMPD);
-            System.out.println("Running time = " + (end - start) + "ms");
+            System.out.println("Running time = " + (end - start) + "ms\n");
+            //-----------------------------------------------------------------------------
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+}
+
+
+class Point implements Comparable<Point> {
+
+    double x, y;
+
+    public int compareTo(Point p) {
+        return Double.compare(this.x, p.x);
+        /*if (this.x == p.x) {
+            return 0;
+        } else if(this.x<p.x){
+            return -1;
+        }
+        else return 1;*/
+    }
+
+    public String toString() {
+        return "(" + x + "," + y + ")";
     }
 
 }
