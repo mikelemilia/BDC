13882fbbf69e758587039c1f59c56e6d52f4339c
Mikele Milia <m.milia@noonic.com>
2020-05-26 10:11:07 +0200

    runsequential method added

--- src/main/java/G05HW3.java
+++ src/main/java/G05HW3.java
@@ -1,2 +1,71 @@
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.Vectors;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class G05HW3 {
+
+    public static void main(String[] args) throws IOException {
+    }
+
+
+    // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+    // METHOD runSequential
+    // Sequential 2-approximation based on matching
+    // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+    public static ArrayList<Vector> runSequential(final ArrayList<Vector> points, int k) {
+
+        final int n = points.size();
+        if (k >= n) {
+            return points;
+        }
+
+        ArrayList<Vector> result = new ArrayList<>(k);
+        boolean[] candidates = new boolean[n];
+        Arrays.fill(candidates, true);
+        for (int iter = 0; iter < k / 2; iter++) {
+            // Find the maximum distance pair among the candidates
+            double maxDist = 0;
+            int maxI = 0;
+            int maxJ = 0;
+            for (int i = 0; i < n; i++) {
+                if (candidates[i]) {
+                    for (int j = i + 1; j < n; j++) {
+                        if (candidates[j]) {
+                            // Use squared euclidean distance to avoid an sqrt computation!
+                            double d = Vectors.sqdist(points.get(i), points.get(j));
+                            if (d > maxDist) {
+                                maxDist = d;
+                                maxI = i;
+                                maxJ = j;
+                            }
+                        }
+                    }
+                }
+            }
+            // Add the points maximizing the distance to the solution
+            result.add(points.get(maxI));
+            result.add(points.get(maxJ));
+            // Remove them from the set of candidates
+            candidates[maxI] = false;
+            candidates[maxJ] = false;
+        }
+        // Add an arbitrary point to the solution, if k is odd.
+        if (k % 2 != 0) {
+            for (int i = 0; i < n; i++) {
+                if (candidates[i]) {
+                    result.add(points.get(i));
+                    break;
+                }
+            }
+        }
+        if (result.size() != k) {
+            throw new IllegalStateException("Result of the wrong size");
+        }
+        return result;
+
+    } // END runSequential
 }
